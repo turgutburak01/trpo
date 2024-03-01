2455f01de418b8629b4e2a5c7a97684e8ae71442
Burak Turgut <turgutburak0144@gmail.com>
2024-03-01 07:03:42 +0300

    update

--- utils.py
+++ utils.py
@@ -21,4 +21,19 @@ def get_flat_params_from(model):
     return flat_params
 
 def set_flat_params_to(model, flat_params):
-    pass
+    prev_ind = []
+    for param in model.parameters():
+        flat_size = int(np.prod(list(param.size())))
+        param.data.copy_(flat_params[prev_ind:prev_ind+flat_size].view(param.size()))
+        prev_ind += flat_size
+        
+def get_flat_grad_from(net, grad_grad=False):
+    grads = []
+    for param in net.parameters():
+        if grad_grad:
+            grads.append(param.grad.grad.view(-1))
+        else:
+            grads.append(param.grad.view(-1))
+            
+        flat_grad = torch.cat(grads)
+        return flat_grad
