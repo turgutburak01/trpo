14ef6c3706f7e78cce8447ee65f32923ce378b6d
Burak Turgut <turgutburak0144@gmail.com>
2024-03-01 08:42:58 +0300

    update

--- main.py
+++ main.py
@@ -83,5 +83,96 @@ def update_params(batch):
         
     targets = Variable(returns)
     
-def get_value_loss(flat_params):
-    pass
+    def get_value_loss(flat_params):
+        set_flat_params_to(value_net, torch.Tensor(flat_params))
+        for param in value_net.parameters():
+            if param.grad is not None:
+                param.grad.data.fill_(0)
+                
+        values_ = value_net(Variable(states))
+        
+        value_loss = (values_ - targets).pow(2).mean()
+        
+        #weight decay
+        for param in value_net.parameters():
+            value_loss += param.pow(2).sum() * args.l2_reg
+        value_loss.backward()
+        return (value_loss.data.double().numpy(), get_flat_grad_from(value_net).data().double().numpy())
+    
+    flat_params, _, opt_info = scipy.optimize.fmin_l_bfgs_b(get_value_loss,
+                                                            get_flat_params_from(value_net).double().numpy(),
+                                                            maxiter=25)
+    set_flat_params_to(value_net, torch.Tensor(flat_params))
+    
+    advantages = (advantages - advantages.mean()) / advantages.std()
+    
+    action_means, action_log_stds, action_stds = policy_net(Variable(states))
+    fixed_log_prob = normal_log_density(Variable(actions), action_means, action_log_stds, action_stds).data.clone()
+    
+    def get_loss(volalite=False):
+        if volalite:
+            with torch.no_grad():
+                action_means, action_log_stds, action_stds = policy_net(Variable(states))
+        else:
+            action_means, action_log_stds, action_stds = policy_net(Variable(states))
+            
+        log_prob = normal_log_density(Variable(actions), action_means, action_log_stds, action_stds)
+        action_loss = -Variable(advantages) * torch.exp(log_prob - Variable(fixed_log_prob))
+        return action_loss.mean()
+    
+    def get_KL():
+        mean1, log_std1, std1 = policy_net(Variable(states))
+        
+        mean0 = Variable(mean1.data)
+        log_std0 = Variable(log_std1.data)
+        std0 = Variable(std1.data)
+        kl = log_std1 - log_std0 + (std0.pow(2) + (mean0 - mean1).pow(2)) / (2.0 * std1.pow(2)) - 0.5
+        return kl.sum(1, keepdim=True)
+    
+    trpo_step(policy_net, get_loss, get_KL, args.max_kl, args.damping)
+    
+running_state = ZFilter((num_inputs,), clip=5)
+running_reward = ZFilter((1,), demean=False, clip=10)
+
+for i_episode in count(1):
+    memory = Memory()
+
+    num_steps = 0
+    reward_batch = 0
+    num_episodes = 0
+    while num_steps < args.batch_size:
+        state = env.reset()
+        state = running_state(state)
+
+        reward_sum = 0
+        for t in range(10000): # Don't infinite loop while learning
+            action = select_action(state)
+            action = action.data[0].numpy()
+            next_state, reward, done, _ = env.step(action)
+            reward_sum += reward
+
+            next_state = running_state(next_state)
+
+            mask = 1
+            if done:
+                mask = 0
+
+            memory.push(state, np.array([action]), mask, next_state, reward)
+
+            if args.render:
+                env.render()
+            if done:
+                break
+
+            state = next_state
+        num_steps += (t-1)
+        num_episodes += 1
+        reward_batch += reward_sum
+
+    reward_batch /= num_episodes
+    batch = memory.sample()
+    update_params(batch)
+
+    if i_episode % args.log_interval == 0:
+        print('Episode {}\tLast reward: {}\tAverage reward {:.2f}'.format(
+            i_episode, reward_sum, reward_batch))
